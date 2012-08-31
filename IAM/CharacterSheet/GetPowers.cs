6d16f62fb6ec4d765258e73fca6242c99a7f3047
Skafte <klaus@lookshy.dk>
2012-08-31 16:28:43 +0200

    getting powers that match sheet powers

--- IAM/CharacterSheet/GetPowers.cs
+++ IAM/CharacterSheet/GetPowers.cs
@@ -18,5 +18,36 @@ namespace IAM.CharacterSheet
 {
    public class GetPowers
    {
+      public void findCharacterPowers()
+      {
+         // get all the power "pages" from the sheet
+         XElement sheetPowerList = new XElement("body", from vPower in Globals.TemporaryData.SelectedCharacterStats.Descendants("powers")
+                                                        select vPower);
+
+
+         XElement ePowers = new XElement("body");
+
+         foreach (XElement eSheetPowerList in Globals.TemporaryData.SelectedCharacterStats.Descendants("powers"))       // get all the power "pages" from the sheet
+         {
+            string powerType = eSheetPowerList.Attribute("type").Value;                                                 // get type of power on page
+            foreach (XElement eFilePowers in Globals.TemporaryData.PowersXML)                                           // search each power file found
+            {
+               if (powerType == eFilePowers.Attribute("type").Value)                      // check if power type from file is the same as the current page
+               {
+                  foreach (XElement eSheetPower in eSheetPowerList.Descendants(powerType))
+                  {
+                     ePowers.Add(from vPower in eFilePowers.Descendants(powerType)
+                                 where ((vPower.Element("name").Value == eSheetPower.Element("name").Value) &&
+                                        (vPower.Element("skill").Element("name").Value == eSheetPower.Element("skill").Value))
+                                 select vPower);
+                  }
+               }
+            }
+         }
+
+
+
+          //Globals.TemporaryData.PowersXML
+      }
    }
 }
